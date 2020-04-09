6ee36d4d236a8c809ff03fcd0686598475ef9359
user <abhay_pratap@thbs.com>
2020-04-09 21:08:39 +0100

    Updates

---
+++ precache-manifest.1d3f8fb0398db74a3f42a112d1433be7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5347deb574803c43fbe6afcb8b06cf2",
+    "url": "/personal-work/index.html"
+  },
+  {
+    "revision": "a99144b5a29bda8e0711",
+    "url": "/personal-work/static/js/2.f50cbe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/personal-work/static/js/2.f50cbe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b5da342b42941d58ab",
+    "url": "/personal-work/static/js/main.10527075.chunk.js"
+  },
+  {
+    "revision": "d5aeed0afa20edac327a",
+    "url": "/personal-work/static/js/runtime-main.fe6190e5.js"
+  }
+]);
